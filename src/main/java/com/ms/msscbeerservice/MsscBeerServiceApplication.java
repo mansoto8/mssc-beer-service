5effeb8a5bc5821377ff3b9b54339f5d0f224ef2
juan <mansoto8@gmail.com>
2021-05-01 15:24:46 -0500

    Project initialization

---
+++ src/main/java/com/ms/msscbeerservice/MsscBeerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ms.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication
+{
+  public static void main(String[] args) {
+    SpringApplication.run(MsscBeerServiceApplication.class, args);
+  }
+}

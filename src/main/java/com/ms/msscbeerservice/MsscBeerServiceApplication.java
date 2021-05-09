50914fca7b5ddb7a4ce3930aefa07368fc64aded
juan <mansoto8@gmail.com>
2021-05-08 19:29:46 -0500

    Adjusted db connection

--- src/main/java/com/ms/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/com/ms/msscbeerservice/MsscBeerServiceApplication.java
@@ -3,9 +3,10 @@ package com.ms.msscbeerservice;
 import com.ms.msscbeerservice.repositories.BeerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscBeerServiceApplication
 {
   public static void main(String[] args) {
